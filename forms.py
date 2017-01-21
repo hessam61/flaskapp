a794b4651d689dc01e2610ddfebd1dc758ea37e5
Unknown <hessam61@gmail.com>
2017-01-21 17:20:47 -0500

    Adds basic validation using WTF Validators

--- forms.py
+++ forms.py
@@ -1,9 +1,10 @@
 from flask_wtf import Form
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
 class SignupForm(Form):
-	first_name = StringField('First Name')
-	last_name = StringField('Last Name')
-	email = StringField('Email')
-	password = PasswordField('Password')
+	first_name = StringField('First Name', validators=[DataRequired("First name is required")])
+	last_name = StringField('Last Name', validators=[DataRequired("Last name is required")])
+	email = StringField('Email', validators=[DataRequired("Email is required")])
+	password = PasswordField('Password', validators=[DataRequired("Password is required")])
 	submit = SubmitField('Sign Up')

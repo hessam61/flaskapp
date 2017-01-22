a2ce75978b95cb9af07739bfa89b3c0da0c0851b
Unknown <hessam61@gmail.com>
2017-01-22 10:15:12 -0500

    min should be an integer

--- forms.py
+++ forms.py
@@ -6,5 +6,5 @@ class SignupForm(Form):
 	first_name = StringField('First Name', validators=[DataRequired("First name is required")])
 	last_name = StringField('Last Name', validators=[DataRequired("Last name is required")])
 	email = StringField('Email', validators=[DataRequired("Email is required"), Email("Please enter a valid email address")])
-	password = PasswordField('Password', validators=[DataRequired("Password is required"), Length(min="6", message="Password should be at least 6 characters long")])
+	password = PasswordField('Password', validators=[DataRequired("Password is required"), Length(min=6, message="Password should be at least 6 characters long")])
 	submit = SubmitField('Sign Up')

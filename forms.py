cff7e0a593c002e316a832fad5f9f329d8e92119
Unknown <hessam61@gmail.com>
2017-01-22 10:09:29 -0500

    Adds email and pass validation

--- forms.py
+++ forms.py
@@ -1,10 +1,10 @@
 from flask_wtf import Form
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, Length
 
 class SignupForm(Form):
 	first_name = StringField('First Name', validators=[DataRequired("First name is required")])
 	last_name = StringField('Last Name', validators=[DataRequired("Last name is required")])
-	email = StringField('Email', validators=[DataRequired("Email is required")])
-	password = PasswordField('Password', validators=[DataRequired("Password is required")])
+	email = StringField('Email', validators=[DataRequired("Email is required"), Email("Please enter a valid email address")])
+	password = PasswordField('Password', validators=[DataRequired("Password is required"), Length(min="6", message="Password should be at least 6 characters long")])
 	submit = SubmitField('Sign Up')

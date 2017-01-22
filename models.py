1d8e6c603aff5de04c17bb33450793c8af661e91
Unknown <hessam61@gmail.com>
2017-01-22 14:06:48 -0500

    adds query model for wiki

--- models.py
+++ models.py
@@ -1,6 +1,10 @@
 from flask.ext.sqlalchemy import SQLAlchemy
 from werkzeug import generate_password_hash, check_password_hash
 
+import geocoder
+import urllib2
+import json
+
 #usable instance of SQLAlchemy class
 db = SQLAlchemy()
 
@@ -25,3 +29,49 @@ class User(db.Model):
 	def check_password(self, password):
 		return check_password_hash(self.pwdhash, password)
 
+# p = Place()
+# places = p.query("1501 walnut st., Philadelphia, PA")
+class Place(object):
+	def meters_to_walking_time(self, meters):
+		# 1 min in roughly 80 meter
+		return int(meters / 80)
+
+	def wiki_path(self, slug):
+		return urllib2.urlparse.urljoin("http://en.wikipedia.org/wiki/", slug.replace(' ', '_'))
+
+	def address_to_latlng(self, address):
+		g = geocoder.google(address)
+		return (g.lat, g.lng)
+
+	def query(self, address):
+		lat, lng = self.address_to_latlng(address)
+
+		query_url = 'https://en.wikipedia.org/w/api.php?action=query&list=geosearch&gsradius=5000&gscoord={0}%7C{1}&gslimit=20&format=json'.format(lat, lng)
+		g = urllib2.urlopen(query_url)
+		results = g.read()
+		g.close()
+
+		data = json.loads(results)
+
+		places = []
+
+		for place in data['query']['geosearch']:
+			name = place['title']
+			meters = place['dist']
+			lat = place['lat']
+			lng = place['lon']
+
+			wiki_url = self.wiki_path(name)
+			walking_time = self.meters_to_walking_time(meters)
+
+			d = {
+				'name': name,
+				'url': wiki_url,
+				'time': walking_time,
+				'lat': lat,
+				'lng': lng
+			}
+
+			places.append(d)
+
+		return places
--- routes.py
+++ routes.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, session, redirect, url_for
-from models import db, User
+from models import db, User, Place
 from forms import SignupForm, LoginForm, AddressForm
 
 app = Flask(__name__)
@@ -71,12 +71,15 @@ def logout():
 	session.pop('email', None)
 	return redirect(url_for('index'))
 
-@app.route("/home")
+@app.route("/home", methods=['GET', 'POST'])
 def home():
 	if 'email' not in session:
 		return redirect(url_for('login'))
 
 	form = AddressForm()
+
+	places = []
+	my_coordinates = (40.019545, -75.209287)
 
 	if request.method == "POST":
 		if form.validate() == "False":
@@ -86,10 +89,14 @@ def home():
 			address = form.address.data
 
 			#query for places around it using Wiki Geodata API
-			pass
+			p = Place()
+			my_coordinates = p.address_to_latlng(address)
+			places = p.query(address)
+			
+			return render_template('home.html', form=form, my_coordinates=my_coordinates, places=places)
 
 	elif request.method == "GET":
-		return render_template('home.html', form=form)
+		return render_template('home.html', form=form, my_coordinates=my_coordinates, places=places)
 
 
 if __name__ == "__main__":
